66af5d8dce8aa6b89512021cc1e20789fe63f82c
Adam C Jones <62266775+adamskiij99@users.noreply.github.com>
2022-10-08 16:52:53 +0100

    Update convex_hull_2d_bm.py

--- convex_hull_2d_bm.py
+++ convex_hull_2d_bm.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Oct  5 16:29:55 2022
+Created on Thu Oct  6 15:49:50 2022
 
 @author: Administrator
 """
@@ -8,12 +8,13 @@ Created on Wed Oct  5 16:29:55 2022
 # Imports
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 from matplotlib.collections import LineCollection
 from scipy.spatial import ConvexHull
 
 # Params
-T = 5
-N = 500000
+T = 2
+N = 20000
 dt = T / N
 sqrtdt = np.sqrt(dt)
 
@@ -23,33 +24,34 @@ dW2 = np.random.normal(0, sqrtdt, N)
 W1 = dW1.cumsum()
 W2 = dW2.cumsum()
 W = np.array([W1, W2]).T
-
-# Generate convex hull and reformat W points for ease later
-hull = ConvexHull(W)
 Wpts = W.reshape(-1, 1, 2)
 segments = np.concatenate([Wpts[:-1], Wpts[1:]], axis=1)
 
-# Formatting
+# Animate
+W1m = W1.min() - 0.1; W1M = W1.max() + 0.1; W2m = W2.min() - 0.1; W2M = W2.max() + 0.1
+def animate(num):
+    ax.clear()
+    plt.xlim([W1m, W1M])
+    plt.ylim([W2m, W2M])
+    lc = LineCollection(segments[: num + 1])
+    lc.set_array(np.linspace(0, 1, N))
+    line = ax.add_collection(lc)
+    # Generate convex hull
+    hull = ConvexHull(W[: max(3, num + 1)]) # Maximum here because ConvexHull requires this size
+    # Plotting convex hull
+    hullCoords = list()
+    for simplex in hull.simplices:
+        hullCoords += list(simplex)
+        plt.plot(W[simplex, 0], W[simplex, 1], "k", linewidth = 0.8)
+    plt.fill(W[hull.vertices, 0], W[hull.vertices, 1], "b", alpha = 0.09)
+
+# Plotting
 plt.style.use("bmh")
-fig = plt.figure(figsize = (12, 12), dpi = 350)
-ax = fig.add_subplot(111)
+fig, ax = plt.subplots(1, 1, figsize = (12, 12))#, dpi = 350)
 ax.set_aspect("equal")
 
-# Colouring/formatting line plot
-lc = LineCollection(segments)
-lc.set_array(np.linspace(0, 1, N))
-lc.set_linewidth(0.5)
-line = ax.add_collection(lc)
-
-# Plotting convex hull
-hullCoords = list()
-for simplex in hull.simplices:
-    hullCoords += list(simplex)
-    plt.plot(W[simplex, 0], W[simplex, 1], "k", linewidth = 0.8)
-plt.fill(W[hull.vertices, 0], W[hull.vertices, 1], "b", alpha = 0.09)
-
-# The BM has already been plotted, this final step is just for axis formatting
-plt.plot(W1, W2, alpha = 0)
-plt.title("Convex hull of Brownian motion up to time T = 5")
+# Saving (requires ffmpeg unless saved as a `.gif`)
+animation_1 = animation.FuncAnimation(fig, animate, interval = 3, frames = N)
+animation_1.save("conv.mp4")
 plt.show()
 
